1a31398317bad4d33315a0ffe638a3fb4e7c981e
Sumit <sumeetbajra@gmail.com>
2016-10-07 00:42:11 +0545

    update user rating

--- routes/ratings.js
+++ routes/ratings.js
@@ -7,7 +7,7 @@ var Movies = require('../models/Movies');
 router.put('/add/:id', checkToken, function(req, res) {
     if(req.body) {
         var rating = req.body;
-        Movies.update({_id: req.params.id}, {$push: {'ratings': rating}}, function(err, doc) {
+        Movies.update({_id: req.params.id}, {$push: {'ratings': {$each: [rating], $position: 0}}}, function(err, doc) {
             if(err) {
                 res.send({error: true, message: err});
             }else {
@@ -16,6 +16,21 @@ router.put('/add/:id', checkToken, function(req, res) {
                 })
             }
         })
+    }
+});
+
+router.put('/update/:id', checkToken, function(req, res) {
+    if(req.body) {
+        var rating = req.body;
+        Movies.update({_id: req.params.id, 'ratings.userId': req.body.userId}, {$set: {'ratings.$.rating': rating.rating, 'ratings.$.review': rating.review}}, function(err, doc) {
+            if(err) {
+                res.send({error: true, message: err});
+            }else {
+                Movies.findOne({_id: req.params.id}).populate('ratings.userId').exec(function(err, doc) {
+                    res.json({error: false, res: doc});
+                })
+            }
+        });
     }
 });
 
